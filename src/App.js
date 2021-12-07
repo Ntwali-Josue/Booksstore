e520e20706ed343e3131df090574ee249480891b
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 14:52:15 +0200

    Render navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Heyy</h1>
+      <Navbar />
     </div>
   );
 }
