cb6f01a23e6e2216f8ac0fbed481ffc23fa14f4d
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 18:32:40 +0200

    Add Links to the books and categories

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="nav">
-      <h1><a href="#"> Bookstore CMS</a></h1>
-      <ul>
-        <li><a href="#">BOOKS</a></li>
-        <li><a href="#">CATEGORIES</a></li>
-      </ul>
+      <h1>Bookstore CMS</h1>
+      <Link to="/books" className="nav-links">Books</Link>
+      <Link to="/categories" className="nav-links">Categories</Link>i
     </nav>
   );
 }
