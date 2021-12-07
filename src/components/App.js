5b1669641595d0fd7090593b9c227d916422dac6
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 17:02:48 +0200

    Render book component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import Navbar from './Navbar';
+import Books from './Books';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <hr />
+      <Books />
     </div>
   );
 }
