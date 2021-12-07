87f2d3e559be891a1fc6bd37732c629addac451f
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 23:44:28 +0200

    Add author input

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Select from 'react-select';
 
 const categories = [
@@ -8,15 +9,23 @@ const categories = [
   { value: '4', label: 'Drama' },
 ];
 
-const InputBook = () => (
-  <form>
-    <h1 style={{ color: '#5c5c5c', padding: '0 50px' }}>Add a new Book</h1>
-    <div className="form-group">
-      <input type="text" className="form-control" id="bookTitle" placeholder="Book Title" />
-      <Select options={categories} className="select" />
-      <input type="submit" value="ADD BOOK" className="add-book" />
-    </div>
-  </form>
-);
+const InputBook = (props) => {
+  const { onChange } = props;
+  return (
+    <form>
+      <h1 style={{ color: '#5c5c5c', padding: '0 50px' }}>Add a new Book</h1>
+      <div className="form-group">
+        <input type="text" className="form-control" id="bookTitle" placeholder="Book Title" />
+        <input type="text" className="form-control" id="bookAuthor" placeholder="Book Author" />
+        <Select options={categories} className="select" />
+        <input type="submit" value="ADD BOOK" className="add-book" onSubmit={onChange} />
+      </div>
+    </form>
+  );
+};
+
+InputBook.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
 
 export default InputBook;
