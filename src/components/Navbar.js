5ada6a187633d5f3f8973ff1194416081631b043
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 15:09:30 +0200

    Add configureStore file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="nav">
-      
+      <h1><a href="#"> Bookstore CMS</a></h1>
+      <ul>
+        <li><a href="#">BOOKS</a></li>
+        <li><a href="#">CATEGORIES</a></li>
+      </ul>
     </nav>
   );
 }
