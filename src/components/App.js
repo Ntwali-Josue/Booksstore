3b7ae9af5a2d9dfbb855bf2e2976fd8282c957a9
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 18:34:43 +0200

    Use Routes and Switch for nav links

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import BookList from './BookLists';
 import InputBook from './InputBook';
+import Book from './Book';
+import Categories from './Categories';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <BookList />
+      <Routes>
+        <Route path="/books" component={Book} />
+        <Route exact path="/categories" component={Categories} />
+      </Routes>
       <InputBook />
     </div>
   );
