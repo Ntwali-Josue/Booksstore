3c67d83c1acdfcc99a9c7867c7e4a8a56b5513c3
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-07 17:57:09 +0200

    Ädd booklists components

---
+++ src/components/BookLists.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BookItem from './BookItem';
+
+const BookList = () => {
+  return (
+    <div>
+      <BookItem />
+      <BookItem />
+      <BookItem />
+      <hr />
+    </div>
+  );
+}
+
+export default BookList;
