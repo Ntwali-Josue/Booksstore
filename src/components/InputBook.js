fd6150ce5b903b66931c0fe905a6740e82468d7e
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-09 17:35:27 +0200

    Remove author input

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
@@ -15,29 +15,17 @@ const InputBook = () => {
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
 
   const handleTitleChange = (e) => {
     e.preventDefault();
     setTitle(e.target.value);
   };
 
-  const handleAuthorChange = (e) => {
-    setAuthor(e.target.value);
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const newBook = {
-      id: uuidv4(),
-      title,
-      author,
-    };
-
-    dispatch(addBook(newBook));
+    dispatch(addBook());
     setTitle('');
-    setAuthor('');
   };
 
   return (
@@ -45,7 +33,6 @@ const InputBook = () => {
       <h1 style={{ color: '#5c5c5c', padding: '0 50px' }}>Add a new Book</h1>
       <div className="form-group">
         <input type="text" className="form-control" id="bookTitle" placeholder="Book Title" value={title} onChange={handleTitleChange} name="title" />
-        <input type="text" className="form-control" id="bookAuthor" placeholder="Book Author" value={author} name="author" onChange={handleAuthorChange} />
         <Select options={categories} className="select" />
         <input type="submit" value="ADD BOOK" className="add-book" />
       </div>
