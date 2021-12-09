e0de4d364744a4773c18b4a4709088e59edcddce
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-09 16:35:19 +0200

    Use effect to get books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Book from './Book';
 import InputBook from './InputBook';
+import { getBooks } from '../redux/books/books';
 
 const BookLists = () => {
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   const books = useSelector((state) => state.booksReducer);
   const mapBooks = books.map((book) => (
-    <Book key={book.id} id={book.id} title={book.title} author={book.author} />
+    <Book key={book.item_id} id={book.item_id} title={book.title} author={book.author} />
   ));
 
   return (
