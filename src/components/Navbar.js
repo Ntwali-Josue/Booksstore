5d11ecfab4f0f639b9a5bde2c205b160a703ca7c
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-10 20:46:11 +0200

    Add profile section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import profile from '../images/profile.png';
 
 const Navbar = () => (
   <nav className="nav">
     <h1>Bookstore CMS</h1>
-    <Link to="/" className="nav-links">Books</Link>
-    <Link to="/categories" className="nav-links">Categories</Link>
+    <div className="links">
+      <Link to="/" className="nav-links">Books</Link>
+      <Link to="/categories" className="nav-links">Categories</Link>
+    </div>
+    <div className="profile">
+      <img src={profile} alt="profile" />
+    </div>
   </nav>
 );
 
